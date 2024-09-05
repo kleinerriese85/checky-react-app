fca9e4b0065a0717eb3e29989b917ed36a976313
kleinerriese85 <143405417+kleinerriese85@users.noreply.github.com>
2024-09-05 15:21:28 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CheckyInterface from './CheckyInterface';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CheckyInterface />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
